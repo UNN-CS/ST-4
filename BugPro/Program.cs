b272a38b0b7e630c170bd228451f869c5ff829b3
edlor-k <korablev.vlm@gmail.com>
2025-04-03 17:03:08 +0300

    feat: first version of project

---
+++ BugPro/Program.cs
@@ -0,0 +1,63 @@
+﻿using Stateless;
+using System;
+
+namespace BugPro {
+    public class Bug {
+        public enum State { Open, Assigned, Defered, Closed, Reopened }
+        private enum Trigger { Assign, Defer, Close, Reopen }
+        private StateMachine<State, Trigger> sm;
+
+        public Bug(State state) {
+            sm = new StateMachine<State, Trigger>(state);
+            sm.Configure(State.Open)
+                .Permit(Trigger.Assign, State.Assigned);
+            sm.Configure(State.Assigned)
+                .Permit(Trigger.Close, State.Closed)
+                .Permit(Trigger.Defer, State.Defered)
+                .Ignore(Trigger.Assign);
+            sm.Configure(State.Closed)
+                .Permit(Trigger.Assign, State.Assigned)
+                .Permit(Trigger.Reopen, State.Reopened);
+            sm.Configure(State.Defered)
+                .Permit(Trigger.Assign, State.Assigned);
+            sm.Configure(State.Reopened)
+                .Permit(Trigger.Assign, State.Assigned);
+        }
+
+        public void Close() {
+            sm.Fire(Trigger.Close);
+            Console.WriteLine("Close");
+        }
+
+        public void Assign() {
+            sm.Fire(Trigger.Assign);
+            Console.WriteLine("Assign");
+        }
+
+        public void Defer() {
+            sm.Fire(Trigger.Defer);
+            Console.WriteLine("Defer");
+        }
+
+        public void Reopen() {
+            sm.Fire(Trigger.Reopen);
+            Console.WriteLine("Reopen");
+        }
+
+        public State getState() {
+            return sm.State;
+        }
+    }
+
+    public class Program {
+        public static void Main(string[] args) {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            bug.Close();
+            bug.Assign();
+            bug.Defer();
+            bug.Assign();
+            Console.WriteLine(bug.getState());
+        }
+    }
+}
