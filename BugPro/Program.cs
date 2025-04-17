7e518af385c6f94bbdeeef909dfb3df41f44c67c
Andrey Chernykh <andreychernykh04@gmail.com>
2025-04-17 19:01:21 +0300

    Add Bug class with tests

--- BugPro/Program.cs
+++ BugPro/Program.cs
@@ -1,48 +1,123 @@
 ﻿using Stateless;
 
-public class Bug {
-    public enum State {Open, Assigned, Defered, Closed}
-    private enum Trigger {Assign, Defer, Close}
-    private StateMachine<State, Trigger> sm;
+namespace BugPro;
 
-    public Bug(State state) {
-        sm = new StateMachine<State, Trigger>(state);
-        sm.Configure(State.Open)
-            .Permit(Trigger.Assign, State.Assigned);
-        sm.Configure(State.Assigned)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Defer, State.Defered)
-            .Ignore(Trigger.Assign);
-        sm.Configure(State.Closed)
-            .Permit(Trigger.Assign, State.Assigned);
-        sm.Configure(State.Defered)
-            .Permit(Trigger.Assign, State.Assigned); 
+public class Bug
+{
+    public enum State
+    {
+        Opened,
+        Assigned,
+        Deferred,
+        Closed,
+        Reviewed,
     }
-    public void Close() {
-        sm.Fire(Trigger.Close);
+
+    private enum Trigger
+    {
+        Assign,
+        Defer,
+        Close,
+        Reopen,
+        Review,
+        Approve,
+        Return,
+        Resume,
+    }
+
+    private readonly StateMachine<State, Trigger> _sm;
+
+    public Bug(State state)
+    {
+        _sm = new StateMachine<State, Trigger>(state);
+
+        _sm.Configure(State.Opened)
+            .Permit(Trigger.Assign, State.Assigned);
+
+        _sm.Configure(State.Assigned)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Defer, State.Deferred)
+            .Permit(Trigger.Review, State.Reviewed);
+
+        _sm.Configure(State.Deferred)
+            .Permit(Trigger.Resume, State.Assigned);
+
+        _sm.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Opened);
+
+        _sm.Configure(State.Reviewed)
+            .Permit(Trigger.Approve, State.Closed)
+            .Permit(Trigger.Return, State.Assigned);
+    }
+
+    public void Assign()
+    {
+        _sm.Fire(Trigger.Assign);
+        Console.WriteLine("Assign");
+    }
+
+    public void Defer()
+    {
+        _sm.Fire(Trigger.Defer);
+        Console.WriteLine("Defer");
+    }
+
+    public void Close()
+    {
+        _sm.Fire(Trigger.Close);
         Console.WriteLine("Close");
     }
-    public void Assign() {
-        sm.Fire(Trigger.Assign);
-        Console.WriteLine("Assign");   
+
+    public void Reopen()
+    {
+        _sm.Fire(Trigger.Reopen);
+        Console.WriteLine("Reopen");
     }
-    public void Defer() {
-        sm.Fire(Trigger.Defer);
-        Console.WriteLine("Defer");   
-    }   
-    public State getState() {
-        return sm.State;
+
+    public void Review()
+    {
+        _sm.Fire(Trigger.Review);
+        Console.WriteLine("Review");
+    }
+
+    public void Approve()
+    {
+        _sm.Fire(Trigger.Approve);
+        Console.WriteLine("Approve");
+    }
+
+    public void Return()
+    {
+        _sm.Fire(Trigger.Return);
+        Console.WriteLine("Return");
+    }
+
+    public void Resume()
+    {
+        _sm.Fire(Trigger.Resume);
+        Console.WriteLine("Resume");
+    }
+
+    public State GetState()
+    {
+        return _sm.State;
     }
 }
 
-public class Program {
-    public static void Main(string[] args) {
-        var bug = new Bug(Bug.State.Open);
+internal static class Program
+{
+    public static void Main()
+    {
+        var bug = new Bug(Bug.State.Opened);
         bug.Assign();
-        bug.Close();
+        bug.Review();
+        bug.Return();
+        bug.Review();
+        bug.Approve();
+        bug.Reopen();
         bug.Assign();
         bug.Defer();
-        bug.Assign();
-        Console.WriteLine(bug.getState());
+        bug.Resume();
+        bug.Close();
     }
 }
