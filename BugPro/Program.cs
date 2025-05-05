42fe417a1f300039004cb4ccbc767e5ddae63aeb
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-05 15:29:34 +0300

    Create Program.cs

---
+++ BugPro/Program.cs
@@ -0,0 +1,88 @@
+﻿using Stateless;
+
+public class Bug {
+    public enum State { Open, Assigned, Defered, Closed, Reopened, Verified }
+    private enum Trigger { Assign, Defer, Close, Reopen, Verify }
+    private StateMachine<State, Trigger> sm;
+
+    public Bug(State state) {
+        sm = new StateMachine<State, Trigger>(state);
+
+        sm.Configure(State.Open)
+            .Permit(Trigger.Assign, State.Assigned);
+
+        sm.Configure(State.Assigned)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Defer, State.Defered)
+            .Ignore(Trigger.Assign);
+
+        sm.Configure(State.Closed)
+            .Permit(Trigger.Assign, State.Assigned);
+
+        sm.Configure(State.Defered)
+            .Permit(Trigger.Assign, State.Assigned);
+
+        sm.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        sm.Configure(State.Reopened)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Close, State.Closed);
+
+        sm.Configure(State.Assigned)
+            .Permit(Trigger.Verify, State.Verified);
+
+        sm.Configure(State.Verified)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Defer, State.Defered);
+    }
+
+    public void Close() {
+        sm.Fire(Trigger.Close);
+        Console.WriteLine("Close");
+    }
+
+    public void Assign() {
+        sm.Fire(Trigger.Assign);
+        Console.WriteLine("Assign");   
+    }
+
+    public void Defer() {
+        sm.Fire(Trigger.Defer);
+        Console.WriteLine("Defer");   
+    }
+
+    public void Reopen() {
+        sm.Fire(Trigger.Reopen);
+        Console.WriteLine("Reopen");
+    }
+
+    public void Verify() {
+        sm.Fire(Trigger.Verify);
+        Console.WriteLine("Verify");
+    }
+
+    public State getState() {
+        return sm.State;
+    }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        var bug = new Bug(Bug.State.Open);
+        bug.Assign();
+        bug.Close();
+        bug.Assign();
+        bug.Defer();
+        bug.Assign();
+        Console.WriteLine(bug.getState());
+
+        var bug2 = new Bug(Bug.State.Open);
+        bug2.Assign();
+        bug2.Verify();
+        bug2.Close();
+        bug2.Reopen();
+        bug2.Assign();
+        Console.WriteLine(bug2.getState());
+    }
+}
